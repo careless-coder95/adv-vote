49403149e23c213e0859d67b3225aa7d31803afd
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-03-21 21:27:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 #  CONFIG — apni values yahan bharo
 # ═══════════════════════════════════════════
 
-BOT_TOKEN   = "YOUR_BOT_TOKEN"       # BotFather se mila token
-MONGO_URI   = "YOUR_MONGODB_URI"     # MongoDB Atlas connection string
+BOT_TOKEN   = "YOUR_BOT_TOKEN"
+MONGO_URI   = "YOUR_MONGODB_URI"
 
 SUPPORT_URL = "https://t.me/CarelessxWorld"
 UPDATE_URL  = "https://t.me/ll_CarelessxCoder_ll"
 OWNER_URL   = "https://t.me/CarelessxOwner"
 
-WELCOME_IMAGE_URL = "YOUR_WELCOME_IMAGE_URL"  # Catbox image URL for /start
+WELCOME_IMAGE_URL = "https://files.catbox.moe/g2rsjz.jpg"  # Catbox image URL for /start
