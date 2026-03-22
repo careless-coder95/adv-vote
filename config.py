92677658a30ee66a7f39ba4637b3a46589d7bfb4
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-03-22 07:30:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+# fill you required variables 
 BOT_TOKEN         = "YOUR_BOT_TOKEN"
 MONGO_URI         = "YOUR_MONGODB_URI"
-WELCOME_IMAGE_URL = "YOUR_WELCOME_IMAGE_URL"
 
-# About page buttons — apne links bharo
+
+# is optional variables, if you want then chnage. If not, the same is also fine।  
+WELCOME_IMAGE_URL = "https://files.catbox.moe/g2rsjz.jpg"
 SUPPORT_URL = "https://t.me/CarelessxWorld"
 UPDATE_URL  = "https://t.me/ll_CarelessxCoder_ll"
 OWNER_URL   = "https://t.me/CarelessxOwner"
