32e367f0ef5b663f8500f26ea14053df5b5565a0
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-03-22 07:27:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+BOT_TOKEN         = "YOUR_BOT_TOKEN"
+MONGO_URI         = "YOUR_MONGODB_URI"
+WELCOME_IMAGE_URL = "YOUR_WELCOME_IMAGE_URL"
+
+# About page buttons — apne links bharo
+SUPPORT_URL = "https://t.me/CarelessxWorld"
+UPDATE_URL  = "https://t.me/ll_CarelessxCoder_ll"
+OWNER_URL   = "https://t.me/CarelessxOwner"
