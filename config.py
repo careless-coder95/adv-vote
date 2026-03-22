242222c5315dffa9beb315d677a247aa3cb3e052
careless-coder95 <dhananjaymaurya3000@gmail.com>
2026-03-22 14:03:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ MONGO_URI         = "YOUR_MONGODB_URI"
 
 
 # is optional variables, if you want then chnage. If not, the same is also fine।  
-WELCOME_IMAGE_URL = "https://files.catbox.moe/g2rsjz.jpg"
+WELCOME_IMAGE_URL = "https://files.catbox.moe/fuk9w5.jpg"
 SUPPORT_URL = "https://t.me/CarelessxWorld"
 UPDATE_URL  = "https://t.me/ll_CarelessxCoder_ll"
 OWNER_URL   = "https://t.me/CarelessxOwner"
